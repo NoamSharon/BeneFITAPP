91da30ffb12c39c1cba980b3b99699a4d321f172
NoamSharon <46422735+NoamSharon@users.noreply.github.com>
2019-02-21 14:37:42 +0200

    test

---
+++ react-proj/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Login from './Login';
+import Home from './Home';
+import { ThemeProvider } from 'react-native-material-ui';
+
+class App extends React.Component {
+    render() {
+      return (
+        <ThemeProvider>
+          <AppNavigator />
+        </ThemeProvider>
+      );
+    }
+  }
+
+  const AppNavigator = createStackNavigator(
+    {
+      Login: Login,
+      Home: Home,
+    },
+    {
+      initialRouteName: 'Home',
+    }
+  );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+});
+
+export default createAppContainer(AppNavigator);
---
+++ react-proj/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, ActionButton } from 'react-native-material-ui';
+
+export default class Home extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home</Text>
+        <Button
+              primary text="go to login page"
+              icon="arrow-back"
+              onPress={() => {
+                this.props.navigation.navigate('Login');
+              }} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+});
