5494c19efcce88005f7871ab3dad62b6939a4622
NoamSharon <46422735+NoamSharon@users.noreply.github.com>
2019-02-23 13:16:04 +0200

    Revert "Revert "noam""
    This reverts commit 7e2c818cb2d2f2debf1242889eaa3e580397020b.

--- react-proj/Home.js
+++ react-proj/Home.js
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home stav noamanoama</Text>
+        <Text>Home stavstst</Text>
         <Button
               primary text="go to login page"
               icon="arrow-back"
