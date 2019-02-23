9277069c3da85e63336c59832c7b2999e6599140
Stav Shalechet <stavs@nanolocksec.com>
2019-02-23 12:48:15 +0200

    commit noam

--- react-proj/Home.js
+++ react-proj/Home.js
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home stav</Text>
+        <Text>Home stav sha</Text>
         <Button
               primary text="go to login page"
               icon="arrow-back"
