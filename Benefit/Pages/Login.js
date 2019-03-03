7b7dc1954a66c572bdc4b76e0132852c4038adc4
Stav Shalechet <stav.sha@gmail.com>
2019-03-03 19:03:02 +0200

    Login

--- Benefit/Pages/Login.js
+++ Benefit/Pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ScrollView, Image, Link } from 'react-native';
+import { Text, ScrollView, Image, Link, ImageBackground } from 'react-native';
 import { RkTextInput, RkButton } from 'react-native-ui-kitten';
 import styles from '../Styles/LoginStyle';
 
@@ -7,67 +7,99 @@ export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.Login = this.Login.bind(this);
+    this.CheckPassword = this.CheckPassword.bind(this);
     this.state = {
       Email: '',
       Password: '',
-      UserCode: ''
+      UserCode: '',
+      IsTrainer: ''
     }
 
   }
 
-  Login() {
-    alert("hi");
-
-    let LoginDetails = {
+  CheckPassword() {
+    const LoginDetails = {
       Email: this.state.Email,
       Password: this.state.Password
     }
-
-    fetch('http://localhost:58400/api/CheckIfEmailExists', {
+    fetch('http://proj.ruppin.ac.il/bgroup79/test1/tar6/api/CheckIfPasswordMatches', {
       method: 'POST',
+      headers: { "Content-type": "application/json; charset=UTF-8" },
       body: JSON.stringify(LoginDetails),
-      headers: { "Content-type": "application/json; charset=UTF-8" }
     })
-      .then(response => response.json())
-      .then(json => {
-        if (json != null) {
-          alert('json=${json}');
+      .then(res => res.json())
+      .then(response => {
+        if (response.UserCode != 0) {
+          this.setState({ UserCode: response.UserCode, IsTrainer: response.IsTrainer });
+          alert('Success!');
         }
-        else { alert('not good') }
+        else
+          alert("Incorrect password");
       })
+
+      .catch(error => console.warn('Error:', error.message));
+  }
+
+
+  Login() {
+    fetch('http://proj.ruppin.ac.il/bgroup79/test1/tar6/api/CheckIfEmailExists?Email=' + this.state.Email, {
+      method: 'POST',
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({}),
+
+    })
+      .then(res => res.json())
+      .then(response => {
+        if (response) {
+          this.CheckPassword();
+        }
+        else alert('Email not exist in the system');
+      })
+
+      .catch(error => console.warn('Error:', error.message));
   }
 
 
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.contentContainer}>
-        <Text>Login</Text>
-        <RkTextInput
-          style={styles.input}
-          rkType='rounded'
-          placeholder='Email'
-          onChangeText={(Email) => this.setState({ Email })}
-        />
-        <RkTextInput style={styles.input}
-          secureTextEntry={true}
-          rkType='rounded'
-          placeholder='Password'
-          onChangeText={(Password) => this.setState({ Password })}
-        />
-        <Text>Not registered yet?
-        <Text
-            style={styles.Signin}
-            onPress={() => {
-              this.props.navigation.navigate('SignIn');
-            }}
-          > Sign in now!</Text></Text>
-        <RkButton
-          style={styles.LoginBtn}
-          onPress={() => this.Login()}
-        >
-          Login
-          </RkButton>
 
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <ImageBackground source={{ uri: 'https://images.pexels.com/photos/421160/pexels-photo-421160.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' }} style={styles.backgroundImage} blurRadius={2}>
+          <Image
+            source={{ uri: 'http://proj.ruppin.ac.il/bgroup79/test1/tar6/Images/Logo.png' }}
+            style={styles.LogoImg} />
+
+
+          <Text style={styles.LoginText}>Login</Text>
+          <RkTextInput style={styles.text}
+            style={styles.input}
+            rkType='rounded'
+            placeholder='Email'
+            // value='stav@gmail.com'
+            onChangeText={(Email) => this.setState({ Email })}
+          />
+          <RkTextInput style={styles.input}
+            secureTextEntry={true}
+            rkType='rounded'
+            placeholder='Password'
+            // value='12345'
+            onChangeText={(Password) => this.setState({ Password })}
+          />
+          <Text style={styles.text} >Not registered yet?
+        <Text
+              style={styles.Signin}
+              onPress={() => {
+                this.props.navigation.navigate('SignIn');
+              }}
+            > Sign in now!</Text></Text>
+
+          <RkButton
+            style={styles.LoginBtn}
+            onPress={() => this.Login()}
+          >
+            Login
+          </RkButton>
+        </ImageBackground>
       </ScrollView>
     );
   }
--- Benefit/Styles/LoginStyle.js
+++ Benefit/Styles/LoginStyle.js
@@ -1,21 +1,46 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     input:
     {
-      width: 350
+        width: 350,
     },
     contentContainer: {
         flex: 1,
         paddingVertical: 20,
+        backgroundColor:'#232A34',
+        paddingVertical: 0
+    },
+    Signin: {
+        color: 'blue',
+
+    },
+    LoginBtn: {
+        borderRadius: 10,
+        backgroundColor: '#05A081',
+        marginTop:15
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
         alignItems: 'center',
-        // justifyContent: 'center',
-        backgroundColor: 'white',
-      },
-      Signin:{
-          color:'blue'
-      },
-      LoginBtn:{
-          borderRadius: 10,
-      }
-  });
+    },
+    text: {
+        marginTop:15,
+        color: 'white'
+        
+    },
+    LogoImg: {
+        width: 100,
+        height: 100,
+        marginTop: 100
+        
+    },
+    LoginText:
+{
+    color: 'white',
+    fontSize: 40
+}
+});
